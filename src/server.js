a703f201cabb0a1d28935a7983728d497b08fd25
Joy M <creator@Life-Is-Joy.local>
2021-08-29 00:20:26 +0900

    ADD: req and res test

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+    return res.send("you can do it Joy! Let's reach where you want to be!!");
+}
+
+const handleLogin = (req, res) => {
+    return res.send("Login here!!!")
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log(`✅Server listening on port http://localhost:${PORT} 📡`);
 
 app.listen(4000, handleListening);
