1bebed830c47ee5e8a23c8442ca1d05d2f9ef282
Joy M <creator@Life-Is-Joy.local>
2021-08-29 00:40:51 +0900

    ADD: middleware test

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
+    console.log("You are here!! finally!!!")
     return res.send("you can do it Joy! Let's reach where you want to be!!");
 }
 
@@ -12,7 +13,23 @@ const handleLogin = (req, res) => {
     return res.send("Login here!!!")
 }
 
-app.get("/", handleHome);
+const one = (req, res, next) => {
+    console.log("middleware one")
+    next()
+}
+
+const two = (req, res, next) => {
+    console.log("middleware two")
+    next()
+}
+
+const three = (req, res, next) => {
+    console.log("middleware three")
+    res.end()
+
+}
+
+app.get("/", one, two, three, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`✅Server listening on port http://localhost:${PORT} 📡`);
